051c1a992004389621775a3f26ce44de978b2fa9
Nirav Ilango <NIlango@esri.com>
2018-07-21 09:29:03 -0700

    login and calendar, partially working

--- app/src/main/java/com/example/nira9586/hack_a_mole/LoginActivity.java
+++ app/src/main/java/com/example/nira9586/hack_a_mole/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.nira9586.hack_a_mole;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,14 +13,31 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 9001;
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
 
 
     @Override
@@ -62,15 +80,22 @@ public class LoginActivity extends AppCompatActivity {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
+            String id = account.getId();
+            myRef.child("users").child(id).child("username").setValue(account.getDisplayName());
+            myRef.child("users").child(id).child("email").setValue(account.getEmail());
+            myRef.child("users").child(id).child("available").setValue("true");
+            myRef.child("users").child(id).child("visible").setValue("true");
+            myRef.child("users").child(id).child("latitude").setValue("34 N");
+            myRef.child("users").child(id).child("longitude").setValue("-117 W");
             // Signed in successfully, show authenticated UI.
-            Intent i = new Intent(LoginActivity.this, MapActivity.class);
+            Intent i = new Intent(LoginActivity.this, CalendarActivity.class);
             startActivity(i);
             finish();
             updateUI(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("0", "signInResult:failed code=" + e.getStatusCode());
+            Log.w("0", "signInResult:failed code=" + e);
             updateUI(null);
         }
     }
